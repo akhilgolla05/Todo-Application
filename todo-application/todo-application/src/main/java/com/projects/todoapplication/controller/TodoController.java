7613d5ecfe940e4b863e0b431b085fe5c72162dc
akhil <gollaakhil1115@gmail.com>
2023-10-31 19:52:15 -0500

    adding folders

--- todo-application/todo-application/src/main/java/com/projects/todoapplication/controller/TodoController.java
+++ todo-application/todo-application/src/main/java/com/projects/todoapplication/controller/TodoController.java
@@ -1,11 +1,15 @@
 package com.projects.todoapplication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
 
+    @ResponseBody
+    @GetMapping("/hello")
     public String getAllTodos()
     {
         return "kanna";
