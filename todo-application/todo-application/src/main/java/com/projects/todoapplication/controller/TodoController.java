95cf5b9bd41b6e0b5e9a415d7a178ddcee5c43f2
akhil <gollaakhil1115@gmail.com>
2023-10-31 19:19:25 -0500

    added controller

---
+++ todo-application/todo-application/src/main/java/com/projects/todoapplication/controller/TodoController.java
@@ -0,0 +1,13 @@
+package com.projects.todoapplication.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+
+    public String getAllTodos()
+    {
+        return "munna";
+    }
+}
